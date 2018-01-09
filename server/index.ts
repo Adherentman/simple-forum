e70f93286abc94ca1e4a5262ebbe82e572c6a891
XuZiHao <18357709818@163.com>
2018-01-09 16:22:47 +0800

    add Apollo-Server

--- server/index.ts
+++ server/index.ts
@@ -1,24 +1,36 @@
 import * as Koa from 'koa';
 import * as mongoose from 'mongoose';
+import * as koaRouter from 'koa-router';
+import * as koaBody from 'koa-bodyparser';
+import { graphiqlKoa } from 'apollo-server-koa';
+
 import env from './env';
 
 const app = new Koa();
+const router = new koaRouter();
 
 const port: number = 8888;
-// 响应
-app.use(ctx => {
-  ctx.body = 'Hello Koa';
-});
+
+app.use(koaBody());
+
+router.get('/graphql',
+  graphiqlKoa({
+    endpointURL: '/graphql'
+  }
+  ));
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+app.listen(port);
+
+console.log("Server is running at port " + port);
 
 mongoose.connect(env.MongoDbUrl);
 let db = mongoose.connection;
 
-db.on('error', function(error){
+db.on('error', function (error) {
   console.log(error);
 });
-db.on('connected', function () {    
-  console.log('Mongoose connection open to ' + env.MongoDbUrl);  
+db.on('connected', function () {
+  console.log('Mongoose connection open to ' + env.MongoDbUrl);
 });  
-
-app.listen(port);
-console.log("Server is running at port " + port );
